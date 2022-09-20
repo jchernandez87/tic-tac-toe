96a04c2a4ea507ea2321f2a1cebd73912baf409f
Juan Carlos <jchernandez827@gmail.com>
2022-09-20 14:58:02 -0500

    add square component style

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,13 +3,11 @@ import Square from "./Square";
 const Board = (props) => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  const squares = arr.map(el => <Square key={el} value={el} onClick={props.onClick} />)
+  const squares = arr.map((el) => (
+    <Square key={el} value={el} onClick={props.onClick} />
+  ));
 
-  return (
-    <div>
-      {squares}
-    </div>
-  );
+  return <div>{squares}</div>;
 };
 
 export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,14 @@
-const Square = (props) => (
-  <button onClick={props.onClick}>
-    {props.value}
-  </button>
-);
+const Square = (props) => {
+  const style = {
+    background: "lightblue",
+    border: "2px solid darkblue",
+    fontSize: "30px",
+    fontWeight: "800",
+    cursor: "pointer",
+    outline: "none",
+  };
+
+  return <button style={style} onClick={props.onClick}>{props.value}</button>;
+};
 
 export default Square;
